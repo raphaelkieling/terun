a08aeba0774d9c75a526f305decb79c01994ea39
Kieling <raphael.kieling@egsys.com.br>
2019-11-21 13:42:29 -0300

    Added todo

--- packages/terun-cli/src/index.ts
+++ packages/terun-cli/src/index.ts
@@ -1,20 +1,26 @@
 import * as Yargs from "yargs";
-import CommandManager from './CommandManager';
+import CommandManager from "./CommandManager";
 import { MakeCommand } from "./commands/MakeCommand";
 import { InitCommand } from "./commands/InitCommand";
 import { AllCommand } from "./commands/AllCommand";
-import { printLogo } from './utils/index';
+import { printLogo } from "./utils/index";
 
-!async function () {
-    await printLogo();
+/**
+ * TODO: Add options like a helper like:
+ *
+ * --make ...
+ * --init ...
+ *
+ * now only exists on document
+ */
+!(async function() {
+  await printLogo();
 
-    const argv = Yargs.argv;
+  const argv = Yargs.argv;
 
-    const manager = new CommandManager()
-    manager.addCommand(new MakeCommand());
-    manager.addCommand(new AllCommand());
-    manager.addCommand(new InitCommand());
-    manager.execute(argv);
-}()
-
-
+  const manager = new CommandManager();
+  manager.addCommand(new MakeCommand());
+  manager.addCommand(new AllCommand());
+  manager.addCommand(new InitCommand());
+  manager.execute(argv);
+})();
