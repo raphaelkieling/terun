16b17d198c858f17302ba1d8186e898c24cebfe3
Raphael Kieling <rapha_kieling@hotmail.com>
2021-03-26 18:12:21 -0300

    :sparkles: Fixed problem with recursive creation folder

--- packages/terun-core/src/utils/file.ts
+++ packages/terun-core/src/utils/file.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as fx from "mkdir-recursive";
+import * as ph from "path";
 
 export function getUtf8File(path: string): string {
   return fs.readFileSync(path, { encoding: "utf-8" });
@@ -9,6 +10,10 @@ export function writeUtf8File(path: string, data: string) {
   return fs.writeFileSync(path, data, { encoding: "utf-8" });
 }
 
+/**
+ * Create a path folders in a recursive way. Allow unix and window so.
+ * @param path
+ */
 export function createDir(path: string): void {
-  fx.mkdirSync(path.substring(0, path.lastIndexOf("/")));
+  fx.mkdirSync(ph.join(...path.split(/[\/\\]/).slice(0, -1)));
 }
