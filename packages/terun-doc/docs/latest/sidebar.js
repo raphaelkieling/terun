450738095df0aa9b1788244e3665148e0ccdda99
Raphael Kieling <rapha_kieling@hotmail.com>
2021-03-26 18:59:04 -0300

    :sparkles: Changed docs

---
+++ packages/terun-doc/docs/latest/sidebar.js
@@ -0,0 +1,28 @@
+module.exports = [
+    '',
+    {
+        title: 'Getting Started',
+        children: [
+            '/latest/guide/what',
+            '/latest/guide/getting-started'
+        ]
+    },
+    {
+        title: 'Reference',
+        children: [
+            '/latest/reference/config',
+            '/latest/reference/commands',
+            '/latest/reference/engine',
+            '/latest/reference/pipelines',
+            '/latest/reference/plugins',
+            '/latest/reference/hooks'
+        ]
+    },
+    {
+        title: 'Plugins',
+        children: [
+            '/latest/plugins/entity',
+            '/latest/plugins/notify'
+        ]
+    }
+]
--- packages/terun-doc/docs/pages/changelog/README.md
+++ packages/terun-doc/docs/pages/changelog/README.md
@@ -10,3 +10,12 @@ The change logs have a simples pattern: `package-modified` What was modified
 - `packages/terun-cli` Fixed some interfaces from core library
 - `packages/terun-cli` Created other logo for terun
 - `all` Fixed some security issues with `npm audit`
+
+### 1.2.1
+
+- `packages/terun-cli` Added a welcome message
+
+### 1.2.2
+
+- `packages/terun-core` Added support for windows and unix path ways.
+- `all` Added a playground folder for CLI tests
